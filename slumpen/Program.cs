82089233ea37a0f467c14a5ef1b9fa7d929cb0ab
antonnn1 <antonnn@vgy.se>
2016-10-28 15:03:34 +0200

    ovn2

--- slumpen/Program.cs
+++ slumpen/Program.cs
@@ -10,7 +10,7 @@ namespace slumpen
     {
         static void Main(string[] args)
         {
-
+            /*
             //Övn 1
             random rand = new random();
             int summa = 0;
@@ -22,19 +22,26 @@ namespace slumpen
                 summa = summa + tal;
             }
             console.writeline("medelvärdet blir: " + (summa / 10));
-
-            /*
+            */
+            
             //Övn 2
             Random rand = new Random();
-            int[] minArray; 
-            
+            int[] minArray = new int[20];
+            int tal;
             for (int i = 0; i < 20; i++)
             {
-                int tal = rand.Next(15, 30);
-                Console.WriteLine(tal);
+                tal = rand.Next(15, 30);
+                Console.Write(tal + " ");
                 minArray[i] = tal;
             }
-            */
+            Array.Sort(minArray);
+            Console.WriteLine();
+            for (int i = 0; i < 20; i++)
+            {
+                Console.Write(minArray[i] + " ");
+            }
+            Console.WriteLine("Medianen blir " + (minArray[8]+minArray[9])/2.0);
+            
         }
     }
 }
